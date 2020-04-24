e1274512f236cace1459c3cef0f761a81acfb9e7
Alex Sykes <alex@alexsykes.net>
2020-04-24 19:37:10 +0100

    File Picker working

--- PadMyTrip/ViewController.swift
+++ PadMyTrip/ViewController.swift
@@ -10,9 +10,10 @@ import UIKit
 import MapKit
 import CoreLocation
 
-class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
+class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate, UIDocumentPickerDelegate {
     // MARK: Outlets
     @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var listBarButtonItem: UIBarButtonItem!
     
     
     // MARK: Properties
@@ -29,7 +30,7 @@ class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDele
         locationManager.delegate = self
         getPermissions()
         setUpMap()
-        
+      //  readFromPublic()
         writeOutputString(dataString: "Some data", fileName: "data", fileExtension: ".csv")
     }
     
@@ -57,20 +58,39 @@ class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDele
     }
     
     // MARK: File Handling
-    func getDocumentsDirectory() -> URL {
-        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
-        // let public = FileManager
-        return paths[0]
+    func readFromPublic () {
+        
+        // open a document picker, select a file
+        // documentTypes see - https://developer.apple.com/library/archive/documentation/Miscellaneous/Reference/UTIRef/Articles/System-DeclaredUniformTypeIdentifiers.html#//apple_ref/doc/uid/TP40009259-SW1
+        
+        let importFileMenu = UIDocumentPickerViewController(documentTypes: ["public.text"],
+                                                            in: UIDocumentPickerMode.open)
+   //     var cancelButton :UIBarButtonItem = UIBarButtonItem!
+      // cancelButton.title = "Cancel"
+        
+        importFileMenu.delegate = self
+        importFileMenu.shouldShowFileExtensions = false
+        importFileMenu.allowsMultipleSelection = true
+      //  importFileMenu.setToolbarItems([cancelButton]?, animated: traitCollection)
+        
+        // importFileMenu.dismiss(animated: true, completion: nil)
+        
+        if #available(iOS 13.0, *) {
+            print("File iOS 13+")
+            importFileMenu.directoryURL = FileManager.default.containerURL(
+                forSecurityApplicationGroupIdentifier: "group.com.alexsykes.MapMyTrip")!
+        } else {
+            // Fallback on earlier versions
+            print("File iOS <=12")
+        }
+        importFileMenu.modalPresentationStyle = .formSheet
+        
+        self.present(importFileMenu, animated: true, completion: nil)
     }
     
-    func createDirectory () {
-        let docDir = self.getDocumentsDirectory()
-        let url = docDir.appendingPathComponent("MyFolderName")
-        do {
-            try FileManager.default.createDirectory(atPath: url.absoluteString, withIntermediateDirectories: true, attributes: nil)
-        } catch let error as NSError {
-            print(error.localizedDescription);
-        }
+    func getDocumentsDirectory() -> URL {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        return paths[0]
     }
     
     func writeOutputString (dataString: String, fileName: String, fileExtension: String) {
@@ -87,6 +107,13 @@ class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDele
         }
     }
     
+    // MARK: Actions
+    @IBAction func fileButtonClicked(_ sender: UIBarButtonItem) {
+        readFromPublic()
+    }
+    
+    // MARK: Delegated methods
+    // Mark: Change of location
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         //mapView.centerToLocation(location)
         let location = locations.last! as CLLocation
@@ -96,5 +123,18 @@ class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDele
             longitudinalMeters: 10000)
         mapView.setRegion(coordinateRegion, animated: true)
         locationManager.stopUpdatingLocation()
+    }
+    
+    // Documents picked
+    func documentPicker(_ controller: UIDocumentPickerViewController,
+                        didPickDocumentsAt urls: [URL]) {
+        let count = urls.count
+        print("\(count) docs selected")
+        
+        for url in urls {
+            let name = url.lastPathComponent
+            print("Filename: \(name)")
+        }
+        return
     }
 }

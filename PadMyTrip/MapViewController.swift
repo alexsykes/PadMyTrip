8e5dc681528f5ba9bf8e50a5c7da13c32cd40794
Alex Sykes <alex@alexsykes.net>
2020-05-01 11:30:00 +0100

    Track input completed

--- PadMyTrip/MapViewController.swift
+++ PadMyTrip/MapViewController.swift
@@ -9,8 +9,9 @@
 import UIKit
 import MapKit
 import CoreLocation
+import Foundation
 
-class MapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
+class MapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate, UIDocumentPickerDelegate  {
     
     // MARK: Properties
     var locationManager: CLLocationManager!
@@ -29,8 +30,9 @@ class MapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerD
         // Do any additional setup after loading the view.
         
         getPermissions()
+        
         setUpMap()
-//        getFileList()
+        //        getFileList()
     }
     
     // Gets permisiions for location services
@@ -44,11 +46,14 @@ class MapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerD
             locationManager.requestWhenInUseAuthorization()
             locationManager.requestAlwaysAuthorization()
         }
-      //  locationManager.startUpdatingLocation()//
-     //   locationManager.startUpdatingHeading()
-               // bgLocation = defaults.bool(forKey: "bgLocation")
+        //  locationManager.startUpdatingLocation()//
+        //   locationManager.startUpdatingHeading()
+        // bgLocation = defaults.bool(forKey: "bgLocation")
     }
     
+    @IBAction func importTracks(_ sender: UIBarButtonItem) {
+                readFromPublic()
+    }
     
     func setUpMap() {
         mapView.mapType = .standard
@@ -56,38 +61,98 @@ class MapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerD
         // mapView.showsCompass = true
     }
     
-//    // MARK: File Handling
-//    func getDocumentsDirectory() -> URL {
-//        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
-//        // let public = FileManager
-//        return paths[0]
-//    }
-//
-//
-//    func getFileList() {
-//        let docDir = try! FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: true)
-//        let skipsHiddenFiles: Bool = true
-//
-//        let URLs = try! FileManager.default.contentsOfDirectory(at: docDir, includingPropertiesForKeys: nil, options: skipsHiddenFiles ? .skipsHiddenFiles : [] )
-//
-//        var csvURLs = URLs.filter{ $0.pathExtension == "csv" || $0.pathExtension == "txt"}
-//        csvURLs.sort(by: { $0.lastPathComponent.lowercased() < $1.lastPathComponent.lowercased() } )
-//
-//     //   let URLs = try! FileManager.default.contentsOfDirectory(at: docDir, includingPropertiesForKeys: nil)
-//        for file in csvURLs {
-//            files.append(file)
-//        }
-//    }
+    //    // MARK: File Handling
+    //    func getDocumentsDirectory() -> URL {
+    //        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+    //        // let public = FileManager
+    //        return paths[0]
+    //    }
+    //
+    //
+    //    func getFileList() {
+    //        let docDir = try! FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: true)
+    //        let skipsHiddenFiles: Bool = true
+    //
+    //        let URLs = try! FileManager.default.contentsOfDirectory(at: docDir, includingPropertiesForKeys: nil, options: skipsHiddenFiles ? .skipsHiddenFiles : [] )
+    //
+    //        var csvURLs = URLs.filter{ $0.pathExtension == "csv" || $0.pathExtension == "txt"}
+    //        csvURLs.sort(by: { $0.lastPathComponent.lowercased() < $1.lastPathComponent.lowercased() } )
+    //
+    //     //   let URLs = try! FileManager.default.contentsOfDirectory(at: docDir, includingPropertiesForKeys: nil)
+    //        for file in csvURLs {
+    //            files.append(file)
+    //        }
+    //    }
     
     
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
+    
+    // Documents picked
+    func documentPicker(_ controller: UIDocumentPickerViewController,
+                        didPickDocumentsAt urls: [URL]) {
+        var trackFiles = urls
+        // Add check for zero return
+        if urls.count == 0 { return}
+        do {
+           try  readReturnedTracks(trackURLs: urls)
+        } catch {
+            print("Error reading returned files: \(error.localizedDescription)")
+        }
     }
-    */
+    
+    func readReturnedTracks(trackURLs :[URL]) throws {
+        let fileManager = FileManager.init()
+        let docDir = try! FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: true)
+        let csvURLs = trackURLs.filter{ $0.pathExtension == "csv"}
+        for trackURL in csvURLs {
+            let filename = trackURL.lastPathComponent
+            let newFileUrl = docDir.appendingPathComponent(filename)
+            do {
+                try  fileManager.copyItem(at: trackURL, to: newFileUrl)
+            } catch {
+                print("Error copying file: \(error.localizedDescription)")
+            }
+        }
+    }
+    
+    // MARK: File Handling
+    /* Presents user with a dialog box u
+     user selects a file or files
+     then the documentPicker didPickDocumentsAt event is fired
+     */
+    // TODO: Add/check Cancel button
+    func readFromPublic () {
+        
+        // open a document picker, select a file
+        // documentTypes see - https://developer.apple.com/library/archive/documentation/Miscellaneous/Reference/UTIRef/Articles/System-DeclaredUniformTypeIdentifiers.html#//apple_ref/doc/uid/TP40009259-SW1
+        
+        let importFileMenu = UIDocumentPickerViewController(documentTypes: ["public.text"],
+                                                            in: UIDocumentPickerMode.import)
+        
+        importFileMenu.delegate = self
+        importFileMenu.shouldShowFileExtensions = true
+        importFileMenu.allowsMultipleSelection = true
+        
+        if #available(iOS 13.0, *) {
+            // print("File iOS 13+")
+            importFileMenu.directoryURL = FileManager.default.containerURL(
+                forSecurityApplicationGroupIdentifier: "group.com.alexsykes.MapMyTrip")!
+        } else {
+            // Fallback on earlier versions
+            //  print("File iOS <=12")
+        }
+        importFileMenu.modalPresentationStyle = .automatic
+        
+        self.present(importFileMenu, animated: true, completion: nil)
+    }
 
 }
--- PadMyTrip/TrackTableViewController.swift
+++ PadMyTrip/TrackTableViewController.swift
@@ -10,121 +10,117 @@ import UIKit
 
 class TrackTableViewController: UITableViewController {
     var files :[URL]! = []
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
-
+        
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem
-            getFileList()
+        getFileList()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(true)
+        getFileList()
+    }
+    
+    
+    // MARK: File Handling
+    func getDocumentsDirectory() -> URL {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        // let public = FileManager
+        return paths[0]
+    }
+    
+    
+    func getFileList() {
+        files.removeAll()
+        let docDir = try! FileManager.default.url(for: .documentDirectory, in: .allDomainsMask, appropriateFor: nil, create: true)
+        let skipsHiddenFiles: Bool = true
+        
+        let URLs = try! FileManager.default.contentsOfDirectory(at: docDir, includingPropertiesForKeys: nil, options: skipsHiddenFiles ? .skipsHiddenFiles : [] )
+        
+        var csvURLs = URLs.filter{ $0.pathExtension == "csv" || $0.pathExtension == "txt"}
+        csvURLs.sort(by: { $0.lastPathComponent.lowercased() < $1.lastPathComponent.lowercased() } )
+        
+        for file in csvURLs {
+            files.append(file)
         }
         
-        
-        // MARK: File Handling
-        func getDocumentsDirectory() -> URL {
-            let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
-            // let public = FileManager
-            return paths[0]
-        }
-        
-        
-        func getFileList() {
-            files.removeAll()
-            let docDir = try! FileManager.default.url(for: .documentDirectory, in: .allDomainsMask, appropriateFor: nil, create: true)
-            let skipsHiddenFiles: Bool = true
-            
-            let URLs = try! FileManager.default.contentsOfDirectory(at: docDir, includingPropertiesForKeys: nil, options: skipsHiddenFiles ? .skipsHiddenFiles : [] )
-            
-            var csvURLs = URLs.filter{ $0.pathExtension == "csv" || $0.pathExtension == "txt"}
-            csvURLs.sort(by: { $0.lastPathComponent.lowercased() < $1.lastPathComponent.lowercased() } )
-                  
-         //   let URLs = try! FileManager.default.contentsOfDirectory(at: docDir, includingPropertiesForKeys: nil)
-            for file in csvURLs {
-                files.append(file)
-            }
-            
-            if files.count == 0 {
-                let dir = docDir.absoluteString
-                let folderURL = docDir.appendingPathComponent("Tracks")
-                do {
-                    try FileManager.default.createDirectory(atPath: folderURL.absoluteString, withIntermediateDirectories: true, attributes: nil)
-                } catch let error as NSError {
-                    print(error.localizedDescription);
-                }
-            }
-        }
-
-
-
+    }
+    
+    
+    
+    
     
     // MARK: - Table view data source
-
+    
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
         return 1
     }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 5 //files.count
+        return files.count
     }
-
+    
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-
+        
         // Configure the cell...
-
+        
         return cell
     }
     
-
+    
     /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
+     // Override to support conditional editing of the table view.
+     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+     // Return false if you do not want the specified item to be editable.
+     return true
+     }
+     */
+    
     /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
+     // Override to support editing the table view.
+     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+     if editingStyle == .delete {
+     // Delete the row from the data source
+     tableView.deleteRows(at: [indexPath], with: .fade)
+     } else if editingStyle == .insert {
+     // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+     }
+     }
+     */
+    
     /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
+     // Override to support rearranging the table view.
+     override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+     
+     }
+     */
+    
     /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
+     // Override to support conditional rearranging of the table view.
+     override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+     // Return false if you do not want the item to be re-orderable.
+     return true
+     }
+     */
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
